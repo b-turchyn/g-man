0ab3a761359790041f2c8255d882439a163032a1
Brian Turchyn <brian@dj-bri-t.net>
2013-10-06 10:58:11 -0500

    Change to tar command

--- gmod_install.sh
+++ gmod_install.sh
@@ -6,7 +6,7 @@ chmod +x steamcmd.sh
 mkdir -p /home/srcds
 useradd srcds
 wget http://media.steampowered.com/client/steamcmd_linux.tar.gz -O /home/srcds/steamcmd_linux.tar.gz
-tar xfz -C /home/srcds /home/srcds/steamcmd_linux.tar.gz
+tar xfz /home/srcds/steamcmd_linux.tar.gz -C /home/srcds 
 chown -R srcds:srcds /home/srcds
 chmod 700 /home/srcds
 su - -c "/home/srcds/steamcmd.sh +login anonymous +force_install_dir /home/srcds/gmod +app_update 4020 validate +force_install_dir /home/srcds/content/css +app_update 232330 validate +force_install_dir /home/srcds/content/tf2 +app_update 232250 validate +quit" srcds
